6dad1575eff89b058b539e6666453a48f4574cba
Loïc Le Coq <loic@xlogo@tuxfamily.org>
2009-01-31 13:05:56 +0000

    ES+CA+EO+FR+EN+IT+GL+DE complete

--- src/xlogo/Config.java
+++ src/xlogo/Config.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
  */
 public class Config {
 	/** Version number**/
-	static final String version="0.9.94 (14 January 2009)";
+	static final String version="0.9.94 (31 January 2009)";
 	/** Drawing Quality **/
 	public static int quality=0;
 	/** Drawing Quality: normal **/
